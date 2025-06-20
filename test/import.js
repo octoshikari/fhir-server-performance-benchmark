b488a5f1a4d7c12dfd51e575486a749759b989d4
Marat Surmashev <muradbei@gmail.com>
2025-06-20 17:02:11 +0200

    CLI WIP

--- test/import.js
+++ test/import.js
@@ -6,12 +6,12 @@ import { headers } from './util.js'
 const bundleSize = new Counter('bundle_size')
 
 export const options = {
-  discardResponseBodies: false,
+  discardResponseBodies: true,
   scenarios: {
     import: {
       executor: 'constant-vus',
       vus: 8,
-      duration: '10m',
+      duration: '5m',
       gracefulStop: '30s',
     },
   },
@@ -21,29 +21,22 @@ export function setup() {
 
   const bundleUrl = __ENV.BUNDLE_URL
   const baseUrl = __ENV.BASE_URL
-  const params = { headers: headers() }
-
+  const params = { headers: headers(), timeout: '300s' }
+  // First - load hospital and practitioner information
   const seeds = ["hospitalInformation.json", "practitionerInformation.json"]
   seeds.forEach(x => {
     const src = http.get(`${bundleUrl}/${x}`)
     http.post(baseUrl, src.body, params)
   })
 
-  return {
-    baseUrl,
-    bundleUrl,
-    params,
-  }
+
+  return { baseUrl, bundleUrl, params, }
 }
 
 export default function ({ baseUrl, bundleUrl, params, }) {
-  const bundle = http.get(
-    bundleUrl,
-    { tags: { group: '::source' } })
-  const x = http.post(
-    baseUrl,
-    bundle.body,
-    { ...params, tags: { group: '::import' } })
+  const bundle = http.get( bundleUrl, { tags: { group: '::source' } })
+  const x = http.post( baseUrl, bundle.body, { ...params, tags: { group: '::import' } })
+
   if (!check(x, { ['Bundle import']: ({ status }) => status === 200 })) return
-  bundleSize.add(x.json().entry.length)
+  bundleSize.add(JSON.parse(bundle.body).entry.length)
 }
