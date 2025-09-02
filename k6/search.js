17364a4f8dad9d7d5f4ceadee9b6e0dad0de4286
Marat Surmashev <muradbei@gmail.com>
2025-09-02 15:38:27 +0200

    Try first search

--- k6/search.js
+++ k6/search.js
@@ -6,8 +6,8 @@ export const options = {
   scenarios: {
     search: {
       executor: 'constant-vus',
-      vus: 50,
-      duration: '5m',
+      vus: 10,
+      duration: '1m',
       gracefulStop: '30s',
     },
   },
@@ -31,28 +31,28 @@ const count = 100
 export default function({ baseUrl, params }) {
   group('text', () => {
     searchTest(baseUrl, 'Patient', `name=John&_count=${count}`, params)
-    searchTest(baseUrl, 'Patient', `name=Undefined&_count=${count}`, params)
-    searchTest(baseUrl, 'Patient', `name=Some_long_unexisting_string&_count=${count}`, params)
-    searchTest(baseUrl, 'Patient', `name:contains=ohn&_count=${count}`, params)
-    searchTest(baseUrl, 'Patient', `given:exact=Cathie710&_count=${count}`, params)
+    // searchTest(baseUrl, 'Patient', `name=Undefined&_count=${count}`, params)
+    // searchTest(baseUrl, 'Patient', `name=Some_long_unexisting_string&_count=${count}`, params)
+    // searchTest(baseUrl, 'Patient', `name:contains=ohn&_count=${count}`, params)
+    // searchTest(baseUrl, 'Patient', `given:exact=Cathie710&_count=${count}`, params)
   })
-  group('date', () => {
-    searchTest(baseUrl, 'Patient', `birthdate=2007-03-07&_count=${count}`, params)
-    searchTest(baseUrl, 'Patient', `birthdate=eq2007-03-07&_count=${count}`, params)
-    searchTest(baseUrl, 'Patient', `birthdate=ne2007-03-07&_count=${count}`, params)
-    searchTest(baseUrl, 'Patient', `birthdate=lt2007-03-07&_count=${count}`, params)
-    searchTest(baseUrl, 'Patient', `birthdate=gt2007-03-07&_count=${count}`, params)
-  })
-  group('token', () => {
-    searchTest(baseUrl, 'Observation', `code=29463-7&_count=${count}`, params)
-    searchTest(baseUrl, 'Observation', `code=http://loinc.org|29463-7&_count=${count}`, params)
-    searchTest(baseUrl, 'Observation', `code=|29463-7&_count=${count}`, params)
-    searchTest(baseUrl, 'Observation', `code=http://loinc.org|&_count=${count}`, params)
-  })
-  group('reference', () => {
-    searchTest(baseUrl, 'Observation', `patient=184cf049-bb4e-91c1-0a44-41c9512eee0c&_count=${count}`, params)
-  })
-  group('parameters', () => {
-    searchTest(baseUrl, 'Patient', `_id=184cf049-bb4e-91c1-0a44-41c9512eee0c&_revinclude=Observation:patient&_count=${count}`, params)
-  })
+  // group('date', () => {
+  //   searchTest(baseUrl, 'Patient', `birthdate=2007-03-07&_count=${count}`, params)
+  //   searchTest(baseUrl, 'Patient', `birthdate=eq2007-03-07&_count=${count}`, params)
+  //   searchTest(baseUrl, 'Patient', `birthdate=ne2007-03-07&_count=${count}`, params)
+  //   searchTest(baseUrl, 'Patient', `birthdate=lt2007-03-07&_count=${count}`, params)
+  //   searchTest(baseUrl, 'Patient', `birthdate=gt2007-03-07&_count=${count}`, params)
+  // })
+  // group('token', () => {
+  //   searchTest(baseUrl, 'Observation', `code=29463-7&_count=${count}`, params)
+  //   searchTest(baseUrl, 'Observation', `code=http://loinc.org|29463-7&_count=${count}`, params)
+  //   searchTest(baseUrl, 'Observation', `code=|29463-7&_count=${count}`, params)
+  //   searchTest(baseUrl, 'Observation', `code=http://loinc.org|&_count=${count}`, params)
+  // })
+  // group('reference', () => {
+  //   searchTest(baseUrl, 'Observation', `patient=184cf049-bb4e-91c1-0a44-41c9512eee0c&_count=${count}`, params)
+  // })
+  // group('parameters', () => {
+  //   searchTest(baseUrl, 'Patient', `_id=184cf049-bb4e-91c1-0a44-41c9512eee0c&_revinclude=Observation:patient&_count=${count}`, params)
+  // })
 }
